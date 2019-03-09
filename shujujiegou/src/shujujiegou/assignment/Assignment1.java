f1f33d8644f107143be1c9d15571be096b5c7d96
lijunjie <853076819@qq.com>
2019-03-09 12:03:51 +0800

    Create Assignment1.java
    Assignment1  initializes the SeqList and shows some usages of the methods in the SeqList Class

---
+++ shujujiegou/src/shujujiegou/assignment/Assignment1.java
@@ -0,0 +1,27 @@
+package shujujiegou.assignment;
+
+import shujujiegou.entity.SeqList;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		SeqList L = new SeqList();
+		
+		//要求一：依次插入13579
+		L.initial();
+/*		//查找适合位置插入新元素x，例如此时插入数据6
+		L.add(6);
+		//插入新元素x后展示数据
+		L.addAndPrint(6);
+		//依次显示顺序表
+		L.printList();
+		//删除第n位数,例如此时删除第2位数
+		L.delList(2);
+		//删除第n位数据后展示数据
+		L.delAndPrint(2);
+		//删除冗余数据
+		L.reduncant();
+		//删除冗余数据展示数据
+		*/
+		
+	}
+}
